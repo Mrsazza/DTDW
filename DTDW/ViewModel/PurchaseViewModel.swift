5e933ca3d81f9f4e3cb87400431fe3a8a958aba4
Sabbir Hossain <sabbirsopnil07@gmail.com>
2025-02-08 16:30:03 +0600

    08|02|2025

---
+++ DTDW/ViewModel/PurchaseViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  PurchaseViewModel.swift
+//  DTDW
+//
+//  Created by Sopnil Sohan on 6/2/25.
+//
+
+import SwiftUI
+
+struct PurchaseViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PurchaseViewModel()
+}
